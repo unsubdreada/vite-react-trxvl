eaa3d80e36e4f20ee32257f09e3970ec0743d863
Alexandr Tulisov <shura74084@gmail.com>
2024-10-28 14:25:59 +0500

    Fix path img

--- src/components/Content/BottomContent/BottomContent.jsx
+++ src/components/Content/BottomContent/BottomContent.jsx
@@ -1,8 +1,10 @@
+import phoneImg from "../../../assets/images/iphone.png";
+
 function BottomContent() {
   return (
     <div className="flex flex-col pt-32 px-36 font-arimo bg-[url(/src/assets/images/bg_bot.png)] bg-cover">
       <div>
-        <img src="/src/assets/images/iphone.png" />
+        <img src={phoneImg} />
         <h1>Your all-in-one travel app.</h1>
         <p>
           Book flights, hotels, trains & rental cars anywhere in the world in
