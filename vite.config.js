8665effc91553c8baedf2e84ac6e991ad961a2f6
Alexandr Tulisov <shura74084@gmail.com>
2024-10-28 13:33:02 +0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-trxvl/",
+  base: "/vite-react-trxvl/",
   plugins: [react()],
 });
